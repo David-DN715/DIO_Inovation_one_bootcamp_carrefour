8eeadc315509e328f4cdd1d39a145517247434b3
David Jose Ferreira do Nascimento <70581471+David-DN715@users.noreply.github.com>
2021-08-03 18:29:43 -0300

    Create classe.py

---
+++ Python/classe.py
@@ -0,0 +1,19 @@
+#Criando um aclasse calculadora!
+class calculador:
+    #Usando o metodo init
+    def __init__(self):
+        pass
+
+    def soma(self , a, b):
+        return a+b
+
+    def sub(self, a, b):
+        return a-b
+
+    def mult(self, a, b):
+        return a*b
+
+    def div(self, a, b):
+        return a/b
+
+calculador = calculador()
