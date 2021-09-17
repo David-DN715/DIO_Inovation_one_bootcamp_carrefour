0dbcb33c276b662c7b0d0332862f0aa82a04e71a
David Jose Ferreira do Nascimento <70581471+David-DN715@users.noreply.github.com>
2021-09-17 14:13:14 -0300

    Create arquivopg_ident.conf.sql

---
+++ postgre.sql/arquivopg_ident.conf.sql
@@ -0,0 +1,4 @@
+#ARquivo responsavel por mapear os usuarios do banco de dados.
+#Localizado no diretorio de dados PGDATA de sua instalação.
+#A opção ident deve ser utilizada no arquivo pg_hba.conf
+
