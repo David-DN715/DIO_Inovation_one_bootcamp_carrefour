c49e1c2360c4fc714ac6a57c21f6d42ffb636af5
David Jose Ferreira do Nascimento <70581471+David-DN715@users.noreply.github.com>
2021-09-17 14:30:45 -0300

    Create Binarios_postegres.sql

---
+++ postgre.sql/Binarios_postegres.sql
@@ -0,0 +1,23 @@
+creatdb
+#Cria o db
+creatuser
+#cria o usuario
+dropuser
+#excluiu o usuario
+dropdb
+#Excluiu o db
+initdb
+#inicia o db
+pg_ctl
+#controla o db
+pg_basebackup
+startaum backup do db
+pg_dump/pg_dumpall
+#estrai em formato de texto as informações daquele momento do seu db
+pg_restore
+#serve para restaurar os arquivos que foi geradopelo dump ou dumpall
+psql
+#é um comando usado para entrar em seu db via systema operacional
+reindexdb
+vacuumdb
+#reorganizar as tabelas do seu db
