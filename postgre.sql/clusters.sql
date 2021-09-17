a230c2dac2405078830b16ab159e3e715e548804
David Jose Ferreira do Nascimento <70581471+David-DN715@users.noreply.github.com>
2021-09-17 14:40:18 -0300

    Create clusters.sql

---
+++ postgre.sql/clusters.sql
@@ -0,0 +1,9 @@
+#Clusters
+Coleção de banco de dados que compartilham as mesmas configurações (Arquivos de confguração) do postgresql e do systema oprecional (porta, listen_addresses, etc)
+
+#Banco de Dados (DATA BASES)
+#conjunto de schemas com seus objetos/relações (Tabelas, funçoes, views, etc)
+
+#SCHEMAS
+#Conjunto de objetos relações (tabelas, funções, views, etc)
+
