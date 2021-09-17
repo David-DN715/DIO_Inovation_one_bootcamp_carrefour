17b92c82764536a1a838032f270afc6f517f7eb2
David Jose Ferreira do Nascimento <70581471+David-DN715@users.noreply.github.com>
2021-09-17 14:20:47 -0300

    Create Comandos_administrativos_ubunto.sql

---
+++ postgre.sql/Comandos_administrativos_ubunto.sql
@@ -0,0 +1,12 @@
+#NO UBUNTO:
+pg_clusters
+#lista todos os clusters PostgreSQL
+
+pg_creatcluster<version><cluster>
+#Cria um novo cluster Postregresql 
+
+pg_dropcluster<version><cluster>
+#apaga um cluster Postgresql
+
+pg_ctlcluster<version><cluster><action>
+#start; stop; status; restart de clusters do postgresql
